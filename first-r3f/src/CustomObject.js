8980437cc8b16866597e4ef2de612e65e57dc20c
Mack Grissom <mcintyregrissom@gmail.com>
2023-01-14 00:52:33 -0500

    inital commit

---
+++ first-r3f/src/CustomObject.js
@@ -0,0 +1,21 @@
+import * as THREE from 'three'
+
+export default function CustomObject() {
+    const verticiesCount = 10 * 3
+    const positions = new Float32Array(verticiesCount * 3)
+
+    for (let i = 0; i < verticiesCount *3; i++)
+    positions[i] = Math.random() - 0.5
+
+   return  <mesh>
+        <bufferGeometry> 
+            <bufferAttribute
+            attach='attributes-position'
+            count={verticiesCount}
+            itemSize={3}
+            array = {positions}
+            />
+         </bufferGeometry> 
+        <meshBasicMaterial color='red'side={THREE.DoubleSide}/>
+    </mesh>
+}
